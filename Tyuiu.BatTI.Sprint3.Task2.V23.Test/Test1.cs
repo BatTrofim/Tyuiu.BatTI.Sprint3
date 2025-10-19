30ee081b9b6cce3461caac358c47be9936426e3f
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-19 15:04:11 +0500

    Task2

---
+++ Tyuiu.BatTI.Sprint3.Task2.V23.Test/Test1.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.BatTI.Sprint3.Task2.V23.Lib;
+
+namespace Tyuiu.BatTI.Sprint3.Task2.V23.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            double value = 1;
+            int startValue = 1;
+            int stopValue = 2;
+
+            double wait = 2.188;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint3.Task2.V23/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.BatTI.Sprint3.Task2.V23.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #3 | Выполнил: Бат Т.И. | ПИНб-25-1";
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* Спринт #3                                                                *");
+Console.WriteLine("* Тема: Цикл do/While                                                      *");
+Console.WriteLine("* Задание #2                                                               *");
+Console.WriteLine("* Вариант #23                                                              *");
+Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+Console.WriteLine("* Написать программу, которая считает сумму ряда                           *");
+Console.WriteLine("*                                                                          *");
+Console.WriteLine("*                                                                          *");
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+Console.WriteLine("****************************************************************************");
+
+double value = Convert.ToDouble(Console.ReadLine());
+int startValue = Convert.ToInt32(Console.ReadLine());
+int stopValue = Convert.ToInt32(Console.ReadLine());
+
+double res = ds.GetSumSeries(value, startValue, stopValue);
+
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+Console.WriteLine("****************************************************************************");
+
+Console.WriteLine("Результат = " + res);
+Console.ReadKey();
