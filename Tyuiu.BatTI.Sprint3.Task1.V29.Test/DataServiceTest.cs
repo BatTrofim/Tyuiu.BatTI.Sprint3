d9f752fe7798bfadef9a02e79dc0e5a7dfca3c47
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-19 14:26:13 +0500

    Task1

---
+++ Tyuiu.BatTI.Sprint3.Task1.V29.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.BatTI.Sprint3.Task1.V29.Lib;
+
+namespace Tyuiu.BatTI.Sprint3.Task1.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            double value = 1;
+            int startValue = 1;
+            int stopValue = 2;
+
+            double wait = 12;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
